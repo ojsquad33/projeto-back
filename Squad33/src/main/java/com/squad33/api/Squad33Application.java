edd3f8125700d61886e729a358c66c1668d38386
Digital <alexandregomesdefarias@gmail.com>
2022-11-01 11:40:37 -0300

    build: Criação do Projeto

---
+++ Squad33/src/main/java/com/squad33/api/Squad33Application.java
@@ -0,0 +1,13 @@
+package com.squad33.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Squad33Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Squad33Application.class, args);
+	}
+
+}
---
+++ Squad33/src/test/java/com/squad33/api/Squad33ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.squad33.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Squad33ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
